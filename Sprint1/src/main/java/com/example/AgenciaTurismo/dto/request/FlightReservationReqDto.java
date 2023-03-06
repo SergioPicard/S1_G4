9b90774a764063806cf8cc2e74d1ef1ce2b771b2
luchofesta <lucianoefesta@hotmail.com>
2023-03-06 13:09:04 -0300

    Actualizacion README

--- Sprint1/src/main/java/com/example/AgenciaTurismo/dto/request/FlightReservationReqDto.java
+++ Sprint1/src/main/java/com/example/AgenciaTurismo/dto/request/FlightReservationReqDto.java
@@ -5,13 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-import java.util.List;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class FlightReservationDto {
+public class FlightReservationReqDto {
     private String userName;
     private FlightReservationResDto flightReservation;
 
--- Sprint1/src/main/java/com/example/AgenciaTurismo/dto/response/FlightResponseDto.java
+++ Sprint1/src/main/java/com/example/AgenciaTurismo/dto/response/FlightResponseDto.java
@@ -1,6 +1,5 @@
 package com.example.AgenciaTurismo.dto.response;
 
-import com.example.AgenciaTurismo.dto.request.FlightReservationDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
