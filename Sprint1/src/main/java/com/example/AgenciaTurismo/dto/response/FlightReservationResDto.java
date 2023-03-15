6ba1dfc15e2b14824fa8a45c4576be48581c2089
luchofesta <lucianoefesta@hotmail.com>
2023-03-15 20:22:36 -0300

    UnitTest Flights e IntegrationTest flightAll

--- Sprint1/src/main/java/com/example/AgenciaTurismo/dto/response/FlightReservationResDto.java
+++ Sprint1/src/main/java/com/example/AgenciaTurismo/dto/response/FlightReservationResDto.java
@@ -3,6 +3,7 @@ package com.example.AgenciaTurismo.dto.response;
 import com.example.AgenciaTurismo.dto.request.PaymentMethodDto;
 import com.example.AgenciaTurismo.dto.request.PeopleDto;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class FlightReservationResDto {
     private LocalDate dateFrom;
     private LocalDate datoTo;
